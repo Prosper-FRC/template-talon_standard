87fa9f46023892e9cc28bb90d72ba47acd93c53d
barbute <jackhdemeterio@gmail.com>
2024-10-05 11:51:07 -0500

    Install libraries, add  README.md, setup logger, add Constants.java

---
+++ README.md
@@ -0,0 +1,2 @@
+# template-talon_standard
+A robot program template for easy programming of a swerve drive and arm powered by KrakenX60 motors.
---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** This class may be used for static (non-changing) global variables */
+public class Constants {
+  public static final int kPilotControllerPort = 0; // Standard joystick port
+}
