bcb5541423fa382a9706c3aee5df89e44be05862
Aryan Kammili <aryan.kammili@gmail.com>
2024-10-19 12:32:07 -0500

    Beam Break Code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # template-talon_standard
-A robot program template for easy programming of a swerve drive and arm powered by KrakenX60 motors.
+
+This template standard contains code for two main types of sensors, Beam Break and Color Sensors. 
+
---
+++ src/main/java/frc/robot/sensors/beamSensor/BeamConstants.java
@@ -0,0 +1,10 @@
+package frc.robot.sensors.beamSensor;
+
+public class BeamConstants {
+    // The DIO port of the beam break port // 
+    public static final int beamBreakPort = 0;
+
+    // Check if the beam break sensor is 'flipped' //
+    // Read the README.md to find out more about 'Flipped' sensors //
+    public static final boolean beamFlipped = false;
+}
