47aa0a56b198f89547c045ad97c764fe3748fdc4
barbute <jackhdemeterio@gmail.com>
2024-10-28 12:19:58 -0500

    Initialize hardware in PivotKrakenHardware.java
    NOTE This commit fails build checks, do not use. I am merely committing to track progress

--- src/main/java/frc/robot/subsystems/pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/pivot/Pivot.java
@@ -42,29 +42,6 @@ public class Pivot extends SubsystemBase {
     }
   }
 
-  private final TalonFX kLeadMotor =
-      PivotConstants.kUseCANBus
-          ? new TalonFX(PivotConstants.kLeaderMotorID, PivotConstants.kCANBusName)
-          : new TalonFX(PivotConstants.kLeaderMotorID);
-  private final TalonFX kFollowMotor =
-      PivotConstants.kUseCANBus
-          ? new TalonFX(PivotConstants.kFollowerMotorID, PivotConstants.kCANBusName)
-          : new TalonFX(PivotConstants.kFollowerMotorID);
-
-  private CANcoder absoluteEncoder;
-
-  private TalonFXConfiguration motorConfiguration = new TalonFXConfiguration();
-
-  // When using these values, call the appropriate getter methods
-  private StatusSignal<Double> internalPositionRotations;
-  private StatusSignal<Double> velocityRotationsPerSec;
-  private List<StatusSignal<Double>> appliedVolts;
-  private List<StatusSignal<Double>> supplyCurrentAmps;
-  private List<StatusSignal<Double>> temperatureCelsius;
-
-  private final VoltageOut kVoltageOut = new VoltageOut(0.0);
-  private final PositionVoltage kPositionVoltage = new PositionVoltage(0.0);
-
   private final ArmFeedforward kFeedforward =
       new ArmFeedforward(
           PivotConstants.kPivotGains.kS(),
@@ -86,75 +63,7 @@ public class Pivot extends SubsystemBase {
   private Rotation2d currentPivotGoalPosition = new Rotation2d();
 
   /** Creates a new Pivot. */
-  public Pivot() {
-    kFollowMotor.setControl(
-        new Follower(
-            PivotConstants.kLeaderMotorID, PivotConstants.kFollowerMotorOpposeMasterDirection));
-
-    // Apply configurations
-    motorConfiguration.Slot0.kP = PivotConstants.kPivotGains.kP();
-    motorConfiguration.Slot0.kI = PivotConstants.kPivotGains.kI();
-    motorConfiguration.Slot0.kD = PivotConstants.kPivotGains.kD();
-
-    motorConfiguration.CurrentLimits.SupplyCurrentLimitEnable =
-        PivotConstants.kMotorConfiguration.kEnableSupplyCurrentLimit();
-    motorConfiguration.CurrentLimits.SupplyCurrentLimit =
-        PivotConstants.kMotorConfiguration.kSupplyCurrentLimitAmps();
-    motorConfiguration.CurrentLimits.StatorCurrentLimitEnable =
-        PivotConstants.kMotorConfiguration.kEnableStatorCurrentLimit();
-    motorConfiguration.CurrentLimits.StatorCurrentLimit =
-        PivotConstants.kMotorConfiguration.kStatorCurrentLimitAmps();
-    motorConfiguration.MotorOutput.NeutralMode = PivotConstants.kMotorConfiguration.kNeutralMode();
-    motorConfiguration.MotorOutput.Inverted =
-        PivotConstants.kMotorConfiguration.kInverted()
-            ? InvertedValue.CounterClockwise_Positive
-            : InvertedValue.Clockwise_Positive;
-    motorConfiguration.Voltage.PeakForwardVoltage =
-        PivotConstants.kMotorConfiguration.kPeakForwardVoltage();
-    motorConfiguration.Voltage.PeakReverseVoltage =
-        PivotConstants.kMotorConfiguration.kPeakReverseVoltage();
-
-    if (PivotConstants.kUseCANCoder) {
-      if (PivotConstants.kUseCANBus) {
-        absoluteEncoder = new CANcoder(PivotConstants.kCANCoderID, PivotConstants.kCANBusName);
-      } else {
-        absoluteEncoder = new CANcoder(PivotConstants.kCANCoderID);
-      }
-      motorConfiguration.Feedback.FeedbackRemoteSensorID = PivotConstants.kCANCoderID;
-      motorConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
-      // This ratio assumes that the CANCoder is mounted directly to your pivot
-      // shaft (with no gears, sprokets, etc. separated the sensor from the shaft)
-      motorConfiguration.Feedback.SensorToMechanismRatio = 1.0 / 1.0;
-    } else {
-      motorConfiguration.Feedback.RotorToSensorRatio = PivotConstants.kGearRatio;
-      motorConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
-    }
-
-    kLeadMotor.getConfigurator().apply(motorConfiguration, 1.0);
-
-    // Get status signals from the motor controller
-    internalPositionRotations = kLeadMotor.getPosition();
-    velocityRotationsPerSec = kLeadMotor.getVelocity();
-    appliedVolts = List.of(kLeadMotor.getMotorVoltage(), kFollowMotor.getMotorVoltage());
-    supplyCurrentAmps = List.of(kLeadMotor.getSupplyCurrent(), kFollowMotor.getSupplyCurrent());
-    temperatureCelsius = List.of(kLeadMotor.getDeviceTemp(), kFollowMotor.getDeviceTemp());
-
-    BaseStatusSignal.setUpdateFrequencyForAll(
-        PivotConstants.kStatusSignalUpdateFrequencyHz,
-        internalPositionRotations,
-        velocityRotationsPerSec,
-        appliedVolts.get(0),
-        appliedVolts.get(1),
-        supplyCurrentAmps.get(0),
-        supplyCurrentAmps.get(1),
-        temperatureCelsius.get(0),
-        temperatureCelsius.get(1));
-
-    // Optimize the CANBus utilization by explicitly telling all CAN signals we
-    // are not using to simply not be sent over the CANBus
-    kLeadMotor.optimizeBusUtilization(0.0, 1.0);
-    kFollowMotor.optimizeBusUtilization(0.0, 1.0);
-  }
+  public Pivot() {}
 
   @Override
   public void periodic() {
@@ -215,30 +124,5 @@ public class Pivot extends SubsystemBase {
   public boolean atGoal() {
     return EqualsUtil.epsilonEquals(
         setpointState.position, currentPivotGoalPosition.getRadians(), 1e-3);
-  }
-
-  @AutoLogOutput(key = "Pivot/Inputs/Position")
-  public Rotation2d getInternnalPosition() {
-    return Rotation2d.fromRotations(internalPositionRotations.getValueAsDouble());
-  }
-
-  @AutoLogOutput(key = "Pivot/Inputs/VelocityRadsPerSec")
-  public double getVelocityRadsPerSec() {
-    return Units.rotationsToRadians(velocityRotationsPerSec.getValueAsDouble());
-  }
-
-  @AutoLogOutput(key = "Pivot/Inputs/AppliedVolts")
-  public double[] getAppliedVolts() {
-    return appliedVolts.stream().mapToDouble(StatusSignal::getValueAsDouble).toArray();
-  }
-
-  @AutoLogOutput(key = "Pivot/Inputs/SupplyCurrentAmps")
-  public double[] getSupplyCurrentAmps() {
-    return supplyCurrentAmps.stream().mapToDouble(StatusSignal::getValueAsDouble).toArray();
-  }
-
-  @AutoLogOutput(key = "Pivot/Inputs/TemperatureCelsius")
-  public double[] getTemperatureCelsius() {
-    return temperatureCelsius.stream().mapToDouble(StatusSignal::getValueAsDouble).toArray();
   }
 }
--- src/main/java/frc/robot/subsystems/pivot/PivotKrakenHardware.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotKrakenHardware.java
@@ -4,15 +4,32 @@
 
 package frc.robot.subsystems.pivot;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.subsystems.pivot.PivotConstants.KrakenConfiguration;
+import java.util.List;
 import org.littletonrobotics.junction.AutoLog;
 
 /**
  * Class that is used to set the control mode of the Kraken and to read telemetry from the motor.
  */
 public class PivotKrakenHardware {
+  /** Set of loggable inputs */
   @AutoLog
   public static class PivotInputs {
+    // "Inputs" is just a fancy term of data from a source. In this case, we are reading telemetry
+    // data from a motor, such as whether it is connected to the CANbus, or what the position of the
+    // motor is.
+
     public boolean leaderMotorConnected = true;
     public boolean followerMotorConnected = true;
 
@@ -22,4 +39,95 @@ public class PivotKrakenHardware {
     public double[] supplyCurrentAmps = new double[] {};
     public double[] temperatureCelsius = new double[] {};
   }
+
+  private final TalonFX kLeadMotor =
+      PivotConstants.kUseCANBus
+          ? new TalonFX(PivotConstants.kLeaderMotorID, PivotConstants.kCANBusName)
+          : new TalonFX(PivotConstants.kLeaderMotorID);
+  private final TalonFX kFollowMotor =
+      PivotConstants.kUseCANBus
+          ? new TalonFX(PivotConstants.kFollowerMotorID, PivotConstants.kCANBusName)
+          : new TalonFX(PivotConstants.kFollowerMotorID);
+
+  private CANcoder absoluteEncoder;
+
+  private TalonFXConfiguration motorConfiguration = new TalonFXConfiguration();
+
+  // When using these values, call the appropriate getter methods
+  private StatusSignal<Double> internalPositionRotations;
+  private StatusSignal<Double> velocityRotationsPerSec;
+  private List<StatusSignal<Double>> appliedVolts;
+  private List<StatusSignal<Double>> supplyCurrentAmps;
+  private List<StatusSignal<Double>> temperatureCelsius;
+
+  private final VoltageOut kVoltageOut = new VoltageOut(0.0);
+  private final PositionVoltage kPositionVoltage = new PositionVoltage(0.0);
+
+  public PivotKrakenHardware(int motorID, KrakenConfiguration configuration) {
+    kFollowMotor.setControl(
+        new Follower(
+            PivotConstants.kLeaderMotorID, PivotConstants.kFollowerMotorOpposeMasterDirection));
+
+    // Apply configurations
+    motorConfiguration.Slot0.kP = PivotConstants.kPivotGains.kP();
+    motorConfiguration.Slot0.kI = PivotConstants.kPivotGains.kI();
+    motorConfiguration.Slot0.kD = PivotConstants.kPivotGains.kD();
+
+    motorConfiguration.CurrentLimits.SupplyCurrentLimitEnable =
+        configuration.kEnableSupplyCurrentLimit();
+    motorConfiguration.CurrentLimits.SupplyCurrentLimit = configuration.kSupplyCurrentLimitAmps();
+    motorConfiguration.CurrentLimits.StatorCurrentLimitEnable =
+        configuration.kEnableStatorCurrentLimit();
+    motorConfiguration.CurrentLimits.StatorCurrentLimit = configuration.kStatorCurrentLimitAmps();
+    motorConfiguration.MotorOutput.NeutralMode = configuration.kNeutralMode();
+    motorConfiguration.MotorOutput.Inverted =
+        configuration.kInverted()
+            ? InvertedValue.CounterClockwise_Positive
+            : InvertedValue.Clockwise_Positive;
+    motorConfiguration.Voltage.PeakForwardVoltage = configuration.kPeakForwardVoltage();
+    motorConfiguration.Voltage.PeakReverseVoltage = configuration.kPeakReverseVoltage();
+
+    if (PivotConstants.kUseCANCoder) {
+      if (PivotConstants.kUseCANBus) {
+        absoluteEncoder = new CANcoder(PivotConstants.kCANCoderID, PivotConstants.kCANBusName);
+      } else {
+        absoluteEncoder = new CANcoder(PivotConstants.kCANCoderID);
+      }
+      motorConfiguration.Feedback.FeedbackRemoteSensorID = PivotConstants.kCANCoderID;
+      motorConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+      // This ratio assumes that the CANCoder is mounted directly to your pivot
+      // shaft (with no gears, sprokets, etc. separated the sensor from the shaft)
+      motorConfiguration.Feedback.SensorToMechanismRatio = 1.0 / 1.0;
+    } else {
+      motorConfiguration.Feedback.RotorToSensorRatio = PivotConstants.kGearRatio;
+      motorConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+    }
+
+    kLeadMotor.getConfigurator().apply(motorConfiguration, 1.0);
+
+    // Get status signals from the motor controller
+    internalPositionRotations = kLeadMotor.getPosition();
+    velocityRotationsPerSec = kLeadMotor.getVelocity();
+    appliedVolts = List.of(kLeadMotor.getMotorVoltage(), kFollowMotor.getMotorVoltage());
+    supplyCurrentAmps = List.of(kLeadMotor.getSupplyCurrent(), kFollowMotor.getSupplyCurrent());
+    temperatureCelsius = List.of(kLeadMotor.getDeviceTemp(), kFollowMotor.getDeviceTemp());
+
+    BaseStatusSignal.setUpdateFrequencyForAll(
+        PivotConstants.kStatusSignalUpdateFrequencyHz,
+        internalPositionRotations,
+        velocityRotationsPerSec,
+        appliedVolts.get(0),
+        appliedVolts.get(1),
+        supplyCurrentAmps.get(0),
+        supplyCurrentAmps.get(1),
+        temperatureCelsius.get(0),
+        temperatureCelsius.get(1));
+
+    // Optimize the CANBus utilization by explicitly telling all CAN signals we
+    // are not using to simply not be sent over the CANBus
+    kLeadMotor.optimizeBusUtilization(0.0, 1.0);
+    kFollowMotor.optimizeBusUtilization(0.0, 1.0);
+  }
+
+  public void updateInputs(PivotInputs inputs) {}
 }
