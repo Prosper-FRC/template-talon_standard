5edbda659f68cff730ab275d51260025f03d885b
Michael Duffy <mduffor@gmail.com>
2024-11-02 12:12:26 -0500

    [MD] bringing Anshul's elevator changes into the base.

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "template-talon_standard";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "22ac13beae03ecd2f9f65ddf6fadff131c0b8869";
+  public static final String GIT_DATE = "2024-10-21 18:36:15 EDT";
+  public static final String GIT_BRANCH = "elevator";
+  public static final String BUILD_DATE = "2024-10-28 18:38:44 EDT";
+  public static final long BUILD_UNIX_TIME = 1730155124813L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
