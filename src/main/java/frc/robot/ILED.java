5e2fd60c61de118cb274d08b60e792c051dc91ea
James Mcnulty <myfatcatmax55@gmail.com>
2024-12-02 17:41:41 -0600

    Template for LED
    this is reference code (not tested working code). This template should be used to get a deeper understanding on how LEDs are implemented.

---
+++ src/main/java/frc/robot/ILED.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+interface ILED{
+
+    public void setRGB(int index, int r, int g, int b);
+    
+    public void setPattern(int length);
+
+    public void stop();
+}
---
+++ src/main/java/frc/robot/LED.java
@@ -0,0 +1,33 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
+public class LED implements ILED {
+
+    AddressableLED m_led;
+    AddressableLEDBuffer m_ledBuffer;
+
+    public LED(int id,int bufferLength){
+        m_led = new AddressableLED(id);
+        m_ledBuffer = new AddressableLEDBuffer(bufferLength);
+    }
+
+    public void setRGB(int index, int r, int g, int b){
+        //set colors
+        m_ledBuffer.setRGB(index,r,g,b);
+        // Set the data
+        m_led.setData(m_ledBuffer);
+        m_led.start();
+    }
+
+    public void setPattern(int length){
+        for(int index = 0; index<length; index++){
+            setRGB(index,0,0,0);
+        }
+    }
+
+    public void stop(){
+        m_led.stop();
+    }
+}
