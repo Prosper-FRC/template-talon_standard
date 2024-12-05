c08e2a1b626225ac089e979368e42892d0574efd
barbute <jackhdemeterio@gmail.com>
2024-12-05 17:01:00 -0600

    Add controller bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,16 +7,28 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.flywheel.Flywheel;
 
 public class RobotContainer {
   private final CommandXboxController kPilotController =
       new CommandXboxController(Constants.kPilotControllerPort);
 
+  private final Flywheel flywheel = new Flywheel();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    kPilotController
+        .y()
+        .whileTrue(Commands.runOnce(() -> flywheel.setVoltage(12.0), flywheel))
+        .whileFalse(Commands.runOnce(() -> flywheel.setVoltage(0.0), flywheel));
+    kPilotController
+        .a()
+        .whileTrue(Commands.runOnce(() -> flywheel.setVoltage(-12.0), flywheel))
+        .whileFalse(Commands.runOnce(() -> flywheel.setVoltage(0.0), flywheel));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
