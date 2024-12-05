db36ec29e1d810dab634a9dddfc41cecff932649
barbute <jackhdemeterio@gmail.com>
2024-12-05 16:46:04 -0600

    Add LoggedTunableNumber.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,4 +7,7 @@ package frc.robot;
 /** This class may be used for static (non-changing) global variables */
 public class Constants {
   public static final int kPilotControllerPort = 0; // Standard joystick port
+  // This affects the behavior of LoggedTunableNumbers, if true then it will listen to NetworkTables
+  // and update based what value is set there, if false then it will use its default value
+  public static final boolean kTuningMode = true;
 }
