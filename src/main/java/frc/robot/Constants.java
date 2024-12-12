0343c833d59e37438540c8000e55ebc762550f80
barbute <jackhdemeterio@gmail.com>
2024-12-12 12:03:52 -0600

    Import flywheel subsystem from AdvantageKit
    Subsystem taken from AK Release v3.2.1 - SwerveDriveProject

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,30 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotBase;
+
 /** This class may be used for static (non-changing) global variables */
 public class Constants {
   public static final int kPilotControllerPort = 0; // Standard joystick port
+
+  /**
+   * The robot mode affects how subsystems are instantiated. REAL: Instantiate the subsystem with
+   * code that runs on the physical robot SIM: Instantiate the subsytem with code that runs in the
+   * simulator REPLAY: Instantiate the subsystem with no hardware since we are getting our hardware
+   * "inputs" from a log file
+   */
+  public static final Mode kRobotMode = (RobotBase.isReal()) ? Mode.REAL : Mode.SIM;
+
+  /** If true LoggedTunableNumber will use values from NT, if false it will use its default value */
+  public static final boolean kTuningMode = true;
+
+  /** The mode that the code should run on */
+  public enum Mode {
+    /** Code running on the physical robot or hardware */
+    REAL,
+    /** Code running in the WPILib simulator */
+    SIM,
+    /** Code being replayed against a log file's inputs */
+    REPLAY
+  }
 }
