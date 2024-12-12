3d0ed598d2126cd3a7b0b4081ac7034a56657caa
AnshulBompada <ancient.flame1145@gmail.com>
2024-12-12 17:46:18 -0600

    Redux sys id and config changes

--- src/main/java/frc/robot/subsystems/drive/SwerveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveConstants.java
@@ -42,7 +42,7 @@ public final class SwerveConstants {
     public static final double kClosedLoopRamp = 0.0;
 
     public static final double kDriveGearRatio = 6.75; // 6.86:1
-    public static final double kAngleGearRatio = 12.8; // 12.8:1
+    public static final double kAngleGearRatio = 150.0 / 7.0; // 12.8:1
 
     public static final SwerveDriveKinematics kSwerveKinematics =
         new SwerveDriveKinematics(
@@ -68,7 +68,7 @@ public final class SwerveConstants {
     public static final double KAzimuthS = 0.0;
 
     /* Drive Motor PID Values */
-    public static final double kDriveP = 1.0;
+    public static final double kDriveP = 2.463;
     public static final double kDriveD = 0.0;
 
     /* Drive Motor Characterization Values */
@@ -77,9 +77,9 @@ public final class SwerveConstants {
     // public static final double driveKV = (2.44 / 12);
     // public static final double driveKA = (0.27 / 12);
     // Use characterization data when possible
-    public static final double kDriveS = (0);
-    public static final double kDriveV = (2.4287);
-    public static final double kDriveA = (0);
+    public static final double kDriveS = (0.0);
+    public static final double kDriveV = (2.6279);
+    public static final double kDriveA = (0.0);
 
     public static final double kPathplanerDriveP = 1.0;
     public static final double kPathplanerDriveI = 1.0;
@@ -100,7 +100,7 @@ public final class SwerveConstants {
     /* Motor Inverts */
     public static final boolean kDriveMotorInvert = false;
     // When using inverted modules for SDS, this value is true
-    public static final boolean kAzimuthMotorInvert = false;
+    public static final boolean kAzimuthMotorInvert = true;
 
     // /* Module Specific Constants */
     public static final ModuleConstants Mod1 =
@@ -108,7 +108,7 @@ public final class SwerveConstants {
           11, // Drive ID
           21, // Azimuth ID
           31, // CANCoder ID
-          Rotation2d.fromRotations(0.374023),
+          Rotation2d.fromRotations(-0.333252+0.5),
           "Front Left");
 
     public static final ModuleConstants Mod2 =
@@ -116,7 +116,7 @@ public final class SwerveConstants {
           12, // Drive ID
           22, // Azimuth ID
           32, // CANCoder ID
-          Rotation2d.fromRotations(-0.312744),
+          Rotation2d.fromRotations(0.316406+0.5),
           "Front Right");
 
     public static final ModuleConstants Mod3 =
@@ -124,7 +124,7 @@ public final class SwerveConstants {
           13, // Drive ID
           23, // Azimuth ID
           33, // CANCoder ID
-          Rotation2d.fromRotations(0.126709),
+          Rotation2d.fromRotations(-0.164307+0.5),
           "Back Left");
 
     public static final ModuleConstants Mod4 =
@@ -132,6 +132,6 @@ public final class SwerveConstants {
           14, // Drive ID
           24, // Azimuth ID
           34, // CANCoder ID
-          Rotation2d.fromRotations(0.217041),
+          Rotation2d.fromRotations(-0.390137+0.5),
           "Back Right");
 }
