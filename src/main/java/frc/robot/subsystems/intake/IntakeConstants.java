9f23147135f943076d14d1eeb046ae394b903d8e
barbute <jackhdemeterio@gmail.com>
2025-01-18 10:12:56 -0600

    Update constants to include correct variables
    NOTE Code does not work in its current state, am currently in process of refactoring

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,20 +1,46 @@
+
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+
 public class IntakeConstants {
-    public static final int kIndexerID = 31;
-    public static final int kIRSensorID = 1;
-    
-    public static final int kIntakeID = 32;
+  public record KrakenConfiguration(
+    boolean kInvert,
+    boolean kEnableStatorCurrentLimit,
+    boolean kEnableSupplyCurrentLimit,
+    double kStatorCurrentLimitAmps,
+    double kSupplyCurrentLimitAmps,
+    double kPeakForwardVoltage,
+    double kPeakReverseVoltage,
+    NeutralModeValue kNeutralMode) {}
 
-    // Trivial as we don't need closed-loop on intake(usually)
-    public static final double kIndexerGearing = 9.0;
-    public static final double kIntakeGearing = 9.0;
+  public record SimulationConfiguration(
+    DCMotor kMotorType,
+    double kMeasurementStdDevs
+  ) {}
 
-    public static final double kWheelCircumference = 1.0;
+  // Taken from mech and electrical
+  public static final int kMotorID = 1;
 
-    public static record IntakeHardwareConfig(int motorID, int irSensorID, boolean motorInvert, double wheelCircumference, double gearing) {
-    }
+  public static final double kGearing = 9.0 / 1.0;
 
-    public static final IntakeHardwareConfig kIntakeConfig = 
-    new IntakeHardwareConfig(kIntakeID, kIRSensorID, false, kWheelCircumference, kIntakeGearing);
+  /** The frequency that telemetry form the motor is pushed to the CANBus */
+  public static final double kStatusSignalUpdateFrequencyHz = 100.0;
+
+  public static final KrakenConfiguration kMotorConfiguration = new KrakenConfiguration(
+    false, 
+    true, 
+    true, 
+    60.0, 
+    45.0, 
+    12.0,
+    -12.0
+    NeutralModeValue.Brake);
+
+  public static final SimulationConfiguration kSimulationConfiguration = new SimulationConfiguration(
+    DCMotor.getKrakenX60(1), 
+    0.0002);
 }
